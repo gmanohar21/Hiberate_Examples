fc78ddeb4d460bfee86920d6e4f79fdc32b65dd4
Manu <gmanohar21@gmail.com>
2021-07-11 15:57:26 +0530

    Transaction+Generator(Increment)+save(-)

---
+++ HB-Proj2-Transaction+Generator/src/main/java/com/mn/test/SaveObject.java
@@ -0,0 +1,53 @@
+package com.mn.test;
+
+import java.io.Serializable;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.mn.entity.Student;
+
+public class SaveObject {
+
+	public static void main(String[] args) {
+		Configuration cfg = null;
+		SessionFactory factory = null;
+		Session ses = null;
+		Student st = null;
+		Transaction tx = null;
+		boolean flag = false;
+		int save;
+
+		cfg = new Configuration();
+		cfg.configure("com/mn/cfgs/Hibernate.cfgs.xml");
+		factory = cfg.buildSessionFactory();
+		ses = factory.openSession();
+		st = new Student();
+		//st.setId(15);//if we use generator to id it will overidden and wont consider id value
+		st.setName("manu");
+		st.setCourse("java");
+
+		System.out.println("tx");
+		try {
+			tx = ses.beginTransaction();// internally calls con.setAutoCommit(false) to begin transaction
+			// save object
+			save = (int) ses.save(st);
+			System.out.println(save);
+			flag = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (flag == true) {
+				tx.commit();
+			} else {
+				tx.rollback();// internally calls con.roollback()
+			}
+			ses.close();
+			factory.close();
+		}
+
+	}
+
+}
