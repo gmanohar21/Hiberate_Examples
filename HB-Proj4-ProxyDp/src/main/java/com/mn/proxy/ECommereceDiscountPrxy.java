a33e01800fc0a3e96249405badf3e2031bee2ac6
Manu <gmanohar21@gmail.com>
2021-07-15 12:41:31 +0530

    Proxy-Design-Pattern

--- HB-Proj4-ProxyDp/src/main/java/com/mn/proxy/ECommereceDiscountPrxy.java
+++ HB-Proj4-ProxyDp/src/main/java/com/mn/proxy/ECommereceDiscountPrxy.java
@@ -8,11 +8,13 @@ public class ECommereceDiscountPrxy implements IECommerce {
 	private IECommerce real;
 	private float discount;
 
+	//if discount dosent comes it goes to real class
 	public ECommereceDiscountPrxy(float discount) {
 		real = new AmazonStore();
 		this.discount = discount;
 	}
 
+	
 	@Override
 	public double shopping(String[] items, double[] prices) {
 		double billAmt=0.0f;
